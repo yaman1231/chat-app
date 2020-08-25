bfb30aa3a566877b0efc70160b4205f017256c2b
yaman1231 <69840526+yaman1231@users.noreply.github.com>
2020-08-25 20:06:03 +0900

    投稿時刻の日本時間への修正

--- config/application.rb
+++ config/application.rb
@@ -14,6 +14,9 @@ module ChatApp
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
     # the framework and any gems in your application.
+    
+    config.i18n.default_locale = :ja
+    config.time_zone = 'Tokyo'
 
     config.generators do |g|
       g.stylesheets false
